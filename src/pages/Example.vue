b7adb8305e10b3f6b345384b2c60e8730ead711e
Yaros <Yaroslavhik@yandex.ua>
2021-11-22 12:30:10 +0100

    first commit

---
+++ src/pages/Example.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+
+    <section>
+      <div class="container">
+        <h1>Example Page</h1>
+      </div>
+
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Example"
+}
+</script>
+
+<style scoped>
+
+</style>
